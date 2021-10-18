898119f949ea8dde38487fc55f36342b11eb405f
miramookerjee8000@gmail.com <miramookerjee8000@gmail.com>
2021-10-18 11:07:40 -0400

    created arrays

--- main.js
+++ main.js
@@ -3,8 +3,35 @@
 function Cat(name, age) {
 	this.name = name;
 	this.age = age;
-	this.image_alt = “Image of a cat”;
-  	this.image = cat.jpg”;
+	this.image_alt = "Image of a cat";
+  	this.image = "cat.jpg";
 }
 
-// Global Variables
+function Dog(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "Image of a dog";
+  this.image = "dog.jpg";
+}
+
+function Frog(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "Image of a frog";
+  this.image = "frog.jpg";
+}
+
+// GLobal Variables 
+var tippy = new Cat("Tippy", 11);
+var snickers = new Dog("Snickers", 20);
+var kermit = new Frog("Kermit", 20);
+
+// Arrays
+var animals = [tippy, snickers, kermit];
+var newNames = ["Jackie", "Nala", "Ribbit"];
+
+// Functions
+function generateRandomIndex(maxIndex) {
+	return Math.floor(Math.random() * maxIndex);
+}
+
